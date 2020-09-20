5525c85691fd0f7f7cf08808533e7133aea96960
Oscar Clemente <oscar.clemente@gmail.com>
2020-09-20 19:16:52 +0200

    Expanded test file.

--- files/foo.hpp
+++ files/foo.hpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include "stdint.h" // we should be using  #include <cstdint>
 
 class foo : IBar
 {
     foo();
     virtual void function(int val);
-    std::string test = R"hello";
-    char c = u'z';
+    virtual bool* function2(int& val);
+    std::string string = "testing \"this\" works";
+    std::string stringWithPrefix= R"hello";
+    char c = 'f';
+    char cWithPrefix = u'z';
+    int num = 2451u;
+    float = .234f;
     // yeah
 };
